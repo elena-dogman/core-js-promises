8f5dae92b9e109c8bc24479335aa884478565a1f
elena-dogman <dogman.choli@gmail.com>
2024-04-15 07:52:04 +0300

    feat: solve queue promises task

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -170,8 +170,16 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let resultPromise = Promise.resolve('');
+
+  promises.forEach((promise) => {
+    resultPromise = resultPromise.then(async (result) => {
+      const value = await promise;
+      return result + value;
+    });
+  });
+  return resultPromise;
 }
 
 module.exports = {
